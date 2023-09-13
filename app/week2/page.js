c7fe8d34c44213dfd4f9ee8570483345ed198a15
Edward Montilla <edward.montilla@edu.sait.ca>
2023-09-12 18:35:24 -0600

    week 2 assignment

---
+++ app/week2/page.js
@@ -0,0 +1,15 @@
+
+export default function Page() {
+    return (
+        <main>
+
+            <div class="font-mono">
+            <h1 style={{ fontSize: 35 }}>My Shopping List</h1>
+            </div>
+            <p> Name: Edward Montilla</p>
+            <p> Course Section: CPRG 306 A</p>
+
+        </main>
+
+    )
+}
