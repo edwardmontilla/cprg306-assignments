3b49fbde388cc0c8650347634811c7a14ddb393b
Edward Montilla <edward.montilla@edu.sait.ca>
2023-10-03 20:46:27 -0600

    Debugged

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -5,10 +5,11 @@ import { useState } from "react";
 
 export default function NewItem() {
     const [name, setName] = useState("");
-    const [quantity , setQuantity] = useState("1");
-    const [category, setCategory] = useState("Produce");
+    const [quantity , setQuantity] = useState("");
+    const [category, setCategory] = useState("");
+    const [itemCreated, setItemCreated] = useState(false);
 
-    const handleSubmit = (event) => { event.preventDefault(); 
+    const handleSubmit = (event) => { event.preventDefault();
     
         const newItem = {
             name,
@@ -16,11 +17,13 @@ export default function NewItem() {
             category,
         };
         console.log(newItem);
-        setEvenCreated(true);
+        setItemCreated(true);
 
         setName("");
         setQuantity("1");
         setCategory("Produce");
+
+        setItemCreated(false);
     };
 
     const handleNameChange = (event) => { setName(event.target.value);
@@ -32,54 +35,76 @@ export default function NewItem() {
     
 
     return (
-                            /* use <input> inside <label> and <option> inside <select> for drop down,
-                                use <button> AND add (tag)="type" or "submit" */
                             
-        <main class="h-screen bg-sky-950">
+        <main>
 
-            <div class="flex justify-center">
-                <div class="w-full max-w-md bg-gray-600 border-2 border-gray-500 p-7 rounded-lg shadow-md mt-10">
-                    <form>
-                        <div class="font-sans pb-7">
+            <div>
+                {itemCreated && (
+                    <div className="absolute top-0 mt-7 font-sans text-slate-100 text-2xl">
+                        Item Created!
+                    </div>
+                )}
 
-                            <h1 class="text-slate-100 text-3xl text-gray-800 font-sans mb-5">Add New Item</h1>
+                <div className="h-full bg-sky-950">
+                    <div className="flex justify-center">
+                        <div className="w-full max-w-md bg-gray-600 border-2 border-gray-500 p-7 rounded-lg shadow-md mt-10">
+                            <form>
+                                <div className="font-sans pb-7">
 
-                            <div>
-                                <p class="text-slate-100 text-m text-gray-800 mb-1">Select Category</p>
+                                    <h1 className="text-slate-100 text-3xl text-gray-800 font-sans mb-5">Add New Item</h1>
 
-                                <select class="text-gray-800 p-1 mb-7 border-zinc-400 border-2 rounded-md" value={category} onChange={handleCategoryChange}>
-                                    <option disabled selected value> </option>
-                                    <option value="Produce" selected>Produce</option>
-                                    <option value="Dairy" selected>Dairy</option>
-                                    <option value="Bakery" selected>Bakery</option>
-                                    <option value="Meat" selected>Meat</option>
-                                    <option value="Frozen Foods" selected>Frozen Foods</option>
-                                    <option value="Canned Foods" selected>Canned Foods</option>
-                                    <option value="Dry Goods" selected>Dry Goods</option>
-                                    <option value="Beverages" selected>Beverages</option>
-                                    <option value="Snacks" selected>Snacks</option>
-                                    <option value="Household" selected>Household</option>
-                                    <option value="Other" selected>Other</option>
+                                    <div>
+                                        <p className="text-slate-100 text-m text-gray-800 mb-1">Select Category</p>
 
-                                </select>
+                                        <select className="text-gray-800 p-1 mb-7 border-zinc-400 border-2 rounded-md"
+                                            value={category}
+                                            onChange={handleCategoryChange}>
+                                                <option value="Produce">Produce</option>
+                                                <option value="Dairy">Dairy</option>
+                                                <option value="Bakery">Bakery</option>
+                                                <option value="Meat">Meat</option>
+                                                <option value="Frozen Foods">Frozen Foods</option>
+                                                <option value="Canned Foods">Canned Foods</option>
+                                                <option value="Dry Goods">Dry Goods</option>
+                                                <option value="Beverages">Beverages</option>
+                                                <option value="Snacks">Snacks</option>
+                                                <option value="Household">Household</option>
+                                                <option value="Other">Other</option>
+                                        </select>
 
-                                <p class="text-slate-100 text-m text-gray-800">Enter Item Name:</p>
-                                <input type="text" placeholder="Apple" required class="text-gray-800 w-4/5 border-zinc-400 border-2 rounded-md p-1 mb-7"
-                                    value={name} onChange={handleNameChange}></input>
-                                
-                                <p class="text-slate-100 text-m text-gray-800">Qty:</p>
-                                <input type="number" min="1" max="120" required class="text-gray-800 w-30 border-zinc-400 border-2 rounded-md p-1 mb-10"
-                                    value={quantity} onChange={handleQuantityChange}></input>
+                                        <p className="text-slate-100 text-m text-gray-800">Enter Item Name:</p>
+                                        <input
+                                            type="text"
+                                            placeholder="Apple"
+                                            required
+                                            className="text-gray-800 w-4/5 border-zinc-400 border-2 rounded-md p-1 mb-7"
+                                            value={name}
+                                            onChange={handleNameChange}
+                                        />
+                                        
+                                        <p className="text-slate-100 text-m text-gray-800">Qty:</p>
+                                        <input
+                                            type="number"
+                                            min="1" max="99"
+                                            required
+                                            className="text-gray-800 w-30 border-zinc-400 border-2 rounded-md p-1 mb-10"
+                                            value={quantity}
+                                            onChange={handleQuantityChange}
+                                        />
 
-                            </div>    
+                                    </div>    
 
-                            <button type="submit" class="bg-slate-700 hover:bg-slate-500 text-slate-100 border-zinc-300 p-2 ml-1 border rounded-md w-15">Add</button>
+                                    <button type="submit" className="bg-slate-700 hover:bg-slate-500 text-slate-100 border-zinc-300 p-2 ml-1 border rounded-md w-15">
+                                        Add
+                                    </button>
 
-                        </div>    
-                    </form>
-                    
+                                </div>    
+                            </form>
+                            
+                        </div>
+
+                    </div>
                 </div>
-
             </div>
 
         </main>
--- app/week4/page.js
+++ app/week4/page.js
@@ -9,8 +9,8 @@ export default function Page() {
             <NewItem />
             </div>
 
-            <div class="absolute left-9 top-8">
-                <button class="text-xs text-white bg-cyan-600 border-2 border-cyan-500 h-8 w-13 max-w-s m-3 py-2 px-3 rounded-lg hover:bg-cyan-700">
+            <div className="absolute left-9 top-8">
+                <button className="text-xs text-white bg-cyan-600 border-2 border-cyan-500 h-8 w-13 max-w-s m-3 py-2 px-3 rounded-lg hover:bg-cyan-700">
                     <Link href="/">Home</Link></button>
  
             </div>
